ce5d6ed108fbc05d79e37d8b8ba4923dca7f9e9a
George Donchev <70439491+GeorgeDonchev@users.noreply.github.com>
2021-03-13 09:46:55 +0200

    change the structure, add functions

--- web_scraping.py
+++ web_scraping.py
@@ -2,27 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+
+def url_to_file(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        content = r.text
+        with open('file.html', 'w') as fl:
+            fl.write(content)
+        return content
+    return ''
+
+
+def parse_to_csv(url):
+    soup = BeautifulSoup(url_to_file (url), 'html.parser')
+    containers = soup.findAll('div', {'class':'item-container'})
+    headers = ['brand', 'price', 'shipping']
+    table=[]
+    for container in containers:
+        title = container.findNext('a', {'class':'item-title'}).text
+        raw_price = container.findNext('li', {'class':'price-current'}).text
+        price = raw_price.split(' ')[0]
+        shipping = container.findNext('li',{'class':'price-ship'}).text
+        table.append([title, price, shipping])
+    db = pd.DataFrame(table, columns = headers)
+    db.to_csv('video-cards.csv', index = True)
+    return
+
+
 url = 'https://www.newegg.com/p/pl?d=graphic+card'
-
-r = requests.get(url)
-content = r.text
-with open('file.html', 'w') as fl:
-    fl.write(content)
-pattern = '("View Details">)([a-zA-Z0-9, -.]+)[<$]'
-
-soup = BeautifulSoup(content, 'html.parser')
-containers = soup.findAll('div', {'class':'item-container'})
-headers = ['brand', 'price', 'shipping']
-table=[]
-for container in containers:
-    current_data = []
-    title = container.findNext('a', {'class':'item-title'}).text
-    raw_price = container.findNext('li', {'class':'price-current'}).text
-    price = raw_price.split(' ')[0]
-    shipping = container.findNext('li',{'class':'price-ship'}).text
-    current_data.append(title)
-    current_data.append(price)
-    current_data.append(shipping)
-    table.append(current_data)
-db = pd.DataFrame(table, columns = headers)
-db.to_csv('video-cards.csv', index = True)
+parse_to_csv(url)
